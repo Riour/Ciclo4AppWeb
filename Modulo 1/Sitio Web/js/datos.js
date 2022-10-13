d342f215f9080a4f99604e894b80c4e4ce724da7
Riour <carrillo.jefferson@gmail.com>
2022-10-13 16:42:33 -0500

    Version_1.0_08102022_min_2:40:15s

---
+++ Modulo 1/Sitio Web/js/datos.js
@@ -0,0 +1,25 @@
+//Crear la función recibir_datos()
+function recibir_datos()
+{
+    var nombre = document.getElementById('nombre').value
+    let correo = document.getElementById('email').value
+    var fecha = document.getElementById('fecha').value
+
+    /*console.log(nombre)
+    console.log(correo)
+    console.log(fecha)*/
+    document.getElementById('txtnombre').value = nombre
+    document.getElementById('txtemail').value = correo
+    document.getElementById('txtfecha').value = fecha
+    alert('Nombre: ' +nombre+'\nCorreo: '+correo+'\nFecha: '+fecha)
+}
+
+function sumar_datos()
+{
+    let num1 = parseInt(document.getElementById('numero1').value)
+    let num2 = parseInt(document.getElementById('numero2').value)
+    let suma = num1 + num2
+    document.getElementById('txtnumero1').value = num1
+    document.getElementById('txtnumero2').value = num2 
+    document.getElementById('txtresultado').value = suma
+}
